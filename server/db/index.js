89c60c5f95408237f025ff13aa54ec97d7261708
Isaiah Simpson <isaiah@beurrage.com>
2013-11-19 17:11:00 -0600

    Server: POST user - add support for adding multiple users at once, improve response formatting.

--- server/db/index.js
+++ server/db/index.js
@@ -15,52 +15,59 @@ module.exports = {
      * Create a new user.
      *
      * @param authUser  The existing user authorizing the creation of the new user.
-     * @param newUser   An object specifying the new user to be created.
+     * @param newUsers  An object or array of objects specifying the new user(s) to be created.
      * @param cb        Callback to be passed the result of the action.
      */
-    createUser: function DBCreateUser (authUser, newUser, cb) {
-        if('firstName' in newUser &&
-            'lastName' in newUser &&
-            'email' in newUser &&
-            'password' in newUser &&
-            'address' in newUser) {
+    createUser: function DBCreateUser (authUser, newUsers, cb) {
+        var results = [],
+            requests = _.isArray(newUsers) ? newUsers : [ newUsers ],
+            done = function (result) {
+                results.push(result);
 
-            bcrypt.hash(newUser.password, 8, function (err, hash) {
-                if(!err) {
-                    dbc.query(sqlTemplates.INSERT_USER, [
-                        newUser.firstName,
-                        newUser.lastName,
-                        newUser.email,
-                        hash,
-                        null,
-                        new Date(),
-                        newUser.address,
-                        newUser.phoneNumber,
-                        1
-                    ], function (err, result) {
-                        if(_.isFunction(cb)) {
-                            if(err) {
-                                cb(err);
-                            } else {
-                                cb(false, result);
-                            }
+                if(results.length === requests.length) {
+                    if(_.isFunction(cb)) {
+                        if(results.length === 1) {
+                            cb.apply(results[0], results[0]);
+                        } else {
+                            cb(false, results);
                         }
-                    });
-
-//                    cb(false, {
-//                        firstName: newUser.firstName,
-//                        lastName: newUser.lastName,
-//                        email: newUser.email,
-//                        createdTimestamp: new Date(),
-//                        address: newUser.address,
-//                        phoneNumber: newUser.phoneNumber
-//                    });
-                } else {
-                    cb('Error hashing password.');
+                    }
                 }
-            });
-        } else if(_.isFunction(cb)) {
-            cb('Missing required information in newUser.');
-        }
+            };
+
+        _.forEach(requests, function (newUser) {
+            if('firstName' in newUser &&
+                'lastName' in newUser &&
+                'email' in newUser &&
+                'password' in newUser &&
+                'address' in newUser) {
+
+                bcrypt.hash(newUser.password, 8, function (err, hash) {
+                    if(!err) {
+                        dbc.query(sqlTemplates.INSERT_USER, [
+                            newUser.firstName,
+                            newUser.lastName,
+                            newUser.email,
+                            hash,
+                            null,
+                            new Date(),
+                            newUser.address,
+                            newUser.phoneNumber,
+                            1
+                        ], function (err, result) {
+                            if(err) {
+                                done([ err ]);
+                            } else {
+                                done([ false, _.extend(result, newUser) ]);
+                            }
+                        });
+                    } else {
+                        done([ 'Error hashing password.' ]);
+                    }
+                });
+            } else {
+                done([ 'Missing required information in newUser.' ]);
+            }
+        });
     }
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,42 @@
-var db = require('../db');
+var db = require('../db'),
+    _ = require('lodash');
 
-exports.create = function (req, res) {
-    db.createUser({}, req.body, function (err, user) {
-        if(err) {
-            res.send(400, {
-                error: err
+module.exports = {
+    create: function createUser (req, res) {
+        if(_.isPlainObject(req.body) || _.isArray(req.body)) {
+            db.createUser('', req.body, function (err, result) {
+                if(err) {
+                    res.send(400, {
+                        error: err
+                    });
+                } else {
+                    var response = [];
+
+                    _.forEach(_.isArray(result) ? result : [ result ], function (rA) {
+                        var result = rA.length === 1 ? null : rA[1],
+                            error = rA[0];
+
+                        if(result) {
+                            response.push({
+                                id: result.insertId,
+                                firstName: result.firstName,
+                                lastName: result.lastName,
+                                email: result.email,
+                                address: result.address,
+                                phoneNumber: 'phoneNumber' in result ? result.phoneNumber : undefined
+                            });
+                        } else {
+                            response.push({
+                                error: error
+                            });
+                        }
+                    });
+
+                    res.send(201, response.length === 1 ? response[0] : response);
+                }
             });
-        } else {
-            res.send(201, user);
         }
-    });
+    }
 };
 
 /*

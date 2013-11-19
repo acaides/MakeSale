b42697060943149802b9baa5dc5e5b6ef9a81a96
Isaiah Simpson <isaiah@beurrage.com>
2013-11-19 17:12:30 -0600

    Server: Update route for users service

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ if ('development' == app.get('env')) {
 
 //app.get('/', routes.index);
 //app.get('/users', user.list);
-app.post('/users', users.create);
+app.post('/services/v1/users', users.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));

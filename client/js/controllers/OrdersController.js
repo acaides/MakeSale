d4aa49051e19e10f723b0189a9e40e8fb84021e1
Isaiah Simpson <isaiah@beurrage.com>
2013-11-22 15:04:40 -0600

    DB: add item_count field to order table. Server: pull item_count in orders query and include in response. Client: show the item count on the orders listing view.

--- client/js/controllers/OrdersController.js
+++ client/js/controllers/OrdersController.js
@@ -2,5 +2,15 @@ define([ './module' ], function (controllers) {
     'use strict';
     controllers.controller('OrdersController', [ '$scope', 'BeurrageNet', function ($, BN) {
         $.orders = BN.getOrders();
+
+        $.itemCountText = function (order) {
+            if(order.itemCount === 0) {
+                return 'no items';
+            } else if(order.itemCount === 1) {
+                return '1 item';
+            } else {
+                return order.itemCount + ' items';
+            }
+        }
     } ]);
 });
